8a7940867f17f4a1bdf9688db456c0e4742e0329
Bill <clintoncsb@gmail.com>
2024-01-29 00:11:31 -0300

    🏗️ feat: initial docker setup

---
+++ app/backend/src/test/java/com/clintonbrito/ibmbootcampproject/IbmBootcampProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clintonbrito.ibmbootcampproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbmBootcampProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
