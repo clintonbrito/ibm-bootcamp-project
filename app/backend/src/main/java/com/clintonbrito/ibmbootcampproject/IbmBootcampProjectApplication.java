783304476a5ef20e51c2076e247b93a809b61e4a
Bill <clintoncsb@gmail.com>
2024-01-31 00:36:19 -0300

    🚧 wip: connecting back-end to front-end

--- app/backend/src/main/java/com/clintonbrito/ibmbootcampproject/IbmBootcampProjectApplication.java
+++ app/backend/src/main/java/com/clintonbrito/ibmbootcampproject/IbmBootcampProjectApplication.java
@@ -3,6 +3,9 @@ package com.clintonbrito.ibmbootcampproject;
 import jakarta.persistence.EntityManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class IbmBootcampProjectApplication {
@@ -11,5 +14,15 @@ public class IbmBootcampProjectApplication {
 		SpringApplication.run(IbmBootcampProjectApplication.class, args);
 		System.out.println("Hello World!");
 	}
+
+//	@Bean
+//	public WebMvcConfigurer corsConfigurer() {
+//		return new WebMvcConfigurer() {
+//			@Override
+//			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/greeting-javaconfig").allowedOrigins("http://localhost:4200");
+//			}
+//		};
+//	}
 
 }
